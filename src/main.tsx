4e892dc492ab3a792cac025d6ddaff7c8c2dadfc
truonggiang1139 <truonggiang1139@gmail.com>
2024-03-16 12:48:01 +0700

    feat: prevent refetch

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,13 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
